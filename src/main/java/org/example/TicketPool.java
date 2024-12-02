a9ba764e846bd046e04d0ed019420cfeabc703eb
Chanuthi02 <chanunuhansa@gmail.com>
2024-12-02 23:13:34 +0530

    updated TicketPool class

--- src/main/java/org/example/TicketPool.java
+++ src/main/java/org/example/TicketPool.java
@@ -1,37 +1,16 @@
 package org.example;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class TicketPool {
-    private final Queue<Ticket> tickets = new LinkedList<>();
-    private final int maxCapacity;
+    private int maxTicketCapacity;
 
-    public TicketPool(int maxCapacity) {
-        this.maxCapacity = maxCapacity;
+    public TicketPool(int maxTicketCapacity) {
+        this.maxTicketCapacity = maxTicketCapacity;
     }
 
-    public synchronized void addTicket(Ticket ticket) throws InterruptedException {
-        while (tickets.size() >= maxCapacity) {
-            wait();
-        }
-        tickets.add(ticket);
-        System.out.println("Vendor added a ticket to the Pool. Current size is " + tickets.size());
-        notifyAll();
+    public void retrieveTicket() {
+        // Simulate customer retrieving a ticket from the pool
+        System.out.println("Customer has retrieved a ticket.");
     }
 
-    public synchronized Ticket removeTicket() throws InterruptedException {
-        while (tickets.isEmpty()) {
-            wait();
-        }
-        Ticket ticket = tickets.poll();
-        System.out.println("Customer has bought a ticket from the pool. Current size is " + tickets.size());
-        notifyAll();
-        return ticket;
-    }
-
-    public synchronized int getTicketCount() {
-        return tickets.size();
-    }
+    // Other methods related to ticket management can go here
 }
-
