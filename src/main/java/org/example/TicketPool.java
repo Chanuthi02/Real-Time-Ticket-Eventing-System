01eab1874070fb21aa0b122794203cf44e5dd150
Chanuthi02 <chanunuhansa@gmail.com>
2024-12-02 23:39:52 +0530

    updated TicketPool class

--- src/main/java/org/example/TicketPool.java
+++ src/main/java/org/example/TicketPool.java
@@ -1,16 +1,39 @@
 package org.example;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TicketPool {
-    private int maxTicketCapacity;
+    private final Queue<Ticket> tickets = new LinkedList<>();
+    private final int maxCapacity;
 
-    public TicketPool(int maxTicketCapacity) {
-        this.maxTicketCapacity = maxTicketCapacity;
+    // Constructor to initialize the TicketPool with a maximum capacity
+    public TicketPool(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
     }
 
-    public void retrieveTicket() {
-        // Simulate customer retrieving a ticket from the pool
-        System.out.println("Customer has retrieved a ticket.");
+    // Add a ticket to the pool
+    public synchronized void addTicket(Ticket ticket) throws InterruptedException {
+        while (tickets.size() >= maxCapacity) {
+            wait();  // Wait if the pool is full
+        }
+        tickets.add(ticket);
+        System.out.println("Vendor added ticket: " + ticket);
+        notifyAll();  // Notify consumers
     }
 
-    // Other methods related to ticket management can go here
+    // Retrieve a ticket from the pool (now accepts customer name)
+    public synchronized void retrieveTicket(String customerName) throws InterruptedException {
+        while (tickets.isEmpty()) {
+            wait();  // Wait if the pool is empty
+        }
+        Ticket ticket = tickets.poll();  // Get the ticket from the pool
+        System.out.println(customerName + " retrieved a ticket: " + ticket);
+        notifyAll();  // Notify vendors to add more tickets
+    }
+
+    // Get the current number of tickets in the pool
+    public synchronized int getTicketCount() {
+        return tickets.size();
+    }
 }
