6d0acd0915e103c01e98be8eefd2403bc011d81b
Chanuthi02 <chanunuhansa@gmail.com>
2024-12-02 13:04:06 +0530

    updated the class Ticket.java

--- src/main/java/org/example/Ticket.java
+++ src/main/java/org/example/Ticket.java
@@ -1,4 +1,22 @@
 package org.example;
 
 public class Ticket {
+    private final int ticketId;
+    private final String eventName;
+    private final double ticketPrice;
+
+    public Ticket(int ticketId, String eventName, double ticketPrice) {
+        this.ticketId = ticketId;
+        this.eventName = eventName;
+        this.ticketPrice = ticketPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{ticketId=" + ticketId +
+                ", eventName='" + eventName + '\'' +
+                ", ticketPrice=" + ticketPrice +
+                '}';
+    }
 }
+
