4d310f0d7b68cf2b9142381ee6e934b68bf70bc9
Chanuthi02 <chanunuhansa@gmail.com>
2024-12-09 20:31:15 +0530

    Updated the TicketPool.java class according to UserInterface.java class

--- src/main/java/org/example/TicketPool.java
+++ src/main/java/org/example/TicketPool.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -11,23 +12,40 @@ public class TicketPool {
         this.maxCapacity = maxCapacity;
     }
 
+    // Add a ticket to the pool
     public synchronized void addTicket(Ticket ticket) throws InterruptedException {
         while (tickets.size() >= maxCapacity) {
-            wait(); // Wait if the pool is full
+            wait();
         }
         tickets.add(ticket);
-        notifyAll(); // Notify consumers
+        notifyAll();
     }
 
+    // Remove a ticket from the pool
     public synchronized Ticket removeTicket() throws InterruptedException {
         while (tickets.isEmpty()) {
-            wait(); // Wait if the pool is empty
+            wait();
         }
         Ticket ticket = tickets.poll();
-        notifyAll(); // Notify producers
+        notifyAll();
         return ticket;
     }
 
+    // Cancel a ticket with a specific ticket ID
+    public synchronized Ticket cancelTicket(int ticketId) {
+        Iterator<Ticket> iterator = tickets.iterator();
+        while (iterator.hasNext()) {
+            Ticket ticket = iterator.next();
+            if (ticket.getTicketId() == ticketId) {
+                iterator.remove();
+                notifyAll(); // Notify producers that a ticket slot is now free
+                return ticket;
+            }
+        }
+        return null; // Return null if no matching ticket is found
+    }
+
+    // Get the current ticket count
     public synchronized int getTicketCount() {
         return tickets.size();
     }
